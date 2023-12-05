06b7a1df99762f95a74f21bfa8c22299cafa0ecc
matias fuentes <matias.fuentes84@gmail.com>
2023-12-05 12:28:17 -0300

    devolucion

--- index.js
+++ index.js
@@ -4,17 +4,23 @@ const elem=new Array(n)
 let promotores=0
 let neutros=0
 let detractores=0
-for(let i=1;i<=n;i++)
+for(let i=0;i<n;i++)
     {   
-        elem[i]=parseInt(prompt("Ingrese la evaluación número "+i+" (notas de 0 a 10)"));
+        elem[i]=parseInt(prompt("Ingrese la evaluación número "+(i+1)+" (notas de 0 a 10)"));
         if(elem[i]>8)promotores++
         else if (elem[i]>6) neutros++
-        else detractores++    S
+        else detractores++ 
     }
-console.log("promotores: "+promotores)
-console.log("detractores: "+promotores)
-let nps=promotores/n-detractores/n
-console.log("NPS: "+nps)
+
+function resultado(){
+    console.log("promotores: "+promotores)
+    console.log("detractores: "+promotores)
+    let nps=promotores/n-detractores/n
+    console.log("NPS: "+nps)
+}
+
+resultado()
+
 
 
 
